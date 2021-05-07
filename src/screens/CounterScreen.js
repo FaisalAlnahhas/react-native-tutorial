3fa819910d7b967c00fc1b7c3493aced2fd388a1
Faisal Alnahhas <falnahhas@headstorm.com>
2021-05-06 21:48:34 -0500

    switched counterscreen to use reducer instead of state

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -2,25 +2,35 @@ import React, { useState } from "react";
 import { useReducer } from "react";
 import { Text, StyleSheet, View, Button } from "react-native";
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { ...state, count: state.count + action.payload };
+    case "decrement":
+      return { ...state, count: state.count - action.payload };
+    default:
+      return state;
+  }
+};
+
 const CounterScreen = () => {
-  let broken_counter = 0; // doesn't work because it does not update the state value
-  const [counter, setCounter] = useState(0); //array destructuring
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
 
   return (
     <View>
       <Button
         title="Increase"
         onPress={() => {
-          setCounter(counter + 1);
+          dispatch({ type: "increment", payload: 1 });
         }}
       />
       <Button
         title="Decrease"
         onPress={() => {
-          setCounter(counter - 1);
+          dispatch({ type: "decrement", payload: 1 });
         }}
       />
-      <Text style={styles.text}> Current count = {counter} </Text>
+      <Text style={styles.text}> Current count = {state.count} </Text>
     </View>
   );
 };
